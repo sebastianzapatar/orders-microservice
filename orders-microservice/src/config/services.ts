91d4c5e74aec2a914265ea2edb3b9e183ad0ffc9
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2024-10-17 10:41:53 -0500

    Con un pie en el otro mundo

--- orders-microservice/src/config/services.ts
+++
@@ -1,7 +0,0 @@
-
-
-
-export const PRODUCT_SERVICE = 'PRODUCT_SERVICE';
-
-
-export const NATS_SERVICE = 'NATS_SERVICE';
--- orders-microservice/src/orders/dto/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './change-order-status.dto';
-export * from './create-order.dto';
-export * from './order-item.dto';
-export * from './update-order.dto';
